5b4a4ae1025beab1f9711863518e8bde0d63504a
iesblancafernandezochoa <iesblancafernandezochoa@gmail.com>
2026-06-06 17:44:49 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,21 +1,12 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+window.firebaseConfig = {
   apiKey: "AIzaSyCq3RwtL7pC71jij_qR_T5kbwX1KLVWoec",
   authDomain: "graduacion-ies.firebaseapp.com",
   projectId: "graduacion-ies",
-  storageBucket: "graduacion-ies.firebasestorage.app",
+  storageBucket: "graduacion-ies.firestorage.app",
   messagingSenderId: "971424459910",
   appId: "1:971424459910:web:69c72475195e0bb927b507",
   measurementId: "G-9S5DEFL193"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+window.NOMBRE_EVENTO = "Graduación · IES Blanca Fernández Ochoa";
